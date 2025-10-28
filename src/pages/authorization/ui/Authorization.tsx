5964b386212630c31f8015833ad43326e9c6692b
echo-tokyo <2two2two8eight@mail.ru>
2025-10-29 01:31:27 +0300

    fix: minor

--- src/pages/authorization/ui/Authorization.tsx
+++ src/pages/authorization/ui/Authorization.tsx
@@ -2,11 +2,7 @@ import { SignIn } from '@/features/authorization'
 import { FC, ReactNode } from 'react'
 
 const Authorization: FC = (): ReactNode => {
-  return (
-    <>
-      <SignIn />
-    </>
-  )
+  return <SignIn />
 }
 
 export default Authorization
