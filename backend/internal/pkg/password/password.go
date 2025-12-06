21c770074adb025dccad63e07ad70d58318048e7
Даниил Дудков <ejyou.user@gmail.com>
2025-12-06 17:57:10 +0300

    Add password strength checker

--- backend/internal/pkg/password/password.go
+++ backend/internal/pkg/password/password.go
@@ -2,6 +2,8 @@
 package password
 
 import (
+	"regexp"
+
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -19,4 +21,28 @@ func Encode(password []byte) ([]byte, error) {
 func IsCorrect(password, hash []byte) bool {
 	err := bcrypt.CompareHashAndPassword(hash, password)
 	return err == nil
+}
+
+// Strong returns true if password is strong. Strength rules:
+// 1) At least 8 letters.
+// 2) At least 1 number.
+// 3) At least 1 upper case.
+// 4) At least 1 special character.
+func Strong(password string) bool {
+	var (
+		matched bool
+		err     error
+		tests   = []string{".{8,}", "[a-z]", "[0-9]", "[A-Z]", "[^\\d\\w]"}
+	)
+
+	for _, test := range tests {
+		matched, err = regexp.MatchString(test, password)
+		if err != nil {
+			return false
+		}
+		if !matched {
+			return false
+		}
+	}
+	return true
 }
