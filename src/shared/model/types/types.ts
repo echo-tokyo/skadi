cc36702ee77087fe059a3176abedeb5980bfd68c
echo-tokyo <2two2two8eight@mail.ru>
2026-04-14 22:39:50 +0300

    fix: minir

--- src/shared/model/types/types.ts
+++ src/shared/model/types/types.ts
@@ -28,14 +28,14 @@ export type TTask = {
   title: string
 }
 
-export type TStatusName = 'Бэклог' | 'В работе' | 'На проверке' | 'Проверено'
-export type TStatusValue = '1' | '2' | '3' | '4'
-export type TStatusId = 1 | 2 | 3 | 4
-
 export type TStatus = {
-  id: TStatusId
-  name: TStatusName
+  id: 1 | 2 | 3 | 4
+  name: 'Бэклог' | 'В работе' | 'На проверке' | 'Проверено'
 }
+
+export type TStatusId = TStatus['id']
+export type TStatusName = TStatus['name']
+export type TStatusValue = `${TStatusId}`
 
 export type TSolution = {
   answer?: string
