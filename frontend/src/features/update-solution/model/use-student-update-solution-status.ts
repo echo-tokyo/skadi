e488303ec199ddaab35c077e22a23c881ec37862
echo-tokyo <2two2two8eight@mail.ru>
2026-04-22 18:02:38 +0300

    fix: useStudentUpdateSolutionStatus formdata

--- frontend/src/features/update-solution/model/use-student-update-solution-status.ts
+++ frontend/src/features/update-solution/model/use-student-update-solution-status.ts
@@ -2,12 +2,18 @@ import { useUpdateSolutionByStudentMutation } from '@/entities/solution'
 import { useMutationAction } from '@/shared/lib'
 import { TStatusId } from '@/shared/model'
 
+// FIXME: status должен быть статусом для ученика: 1 | 2 | 3
+type TInput = { id: number; status: TStatusId }
+
+const prepare = ({ id, status }: TInput): { id: number; data: FormData } => {
+  const fd = new FormData()
+  fd.append('status_id', String(Number(status)))
+  return { id, data: fd }
+}
+
 export const useStudentUpdateSolutionStatus = () => {
   return useMutationAction({
     mutation: useUpdateSolutionByStudentMutation(),
-    prepare: ({ id, status_id }: { id: number; status_id: TStatusId }) => ({
-      id,
-      data: { status_id },
-    }),
+    prepare,
   })
 }
