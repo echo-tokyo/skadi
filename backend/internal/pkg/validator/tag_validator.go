d7b8381cb28b92e1c0b03dead1e88c8451baf019
Даниил Дудков <ejyou.user@gmail.com>
2026-05-04 12:17:40 +0300

    Add: custom validation tag - strong-passwd

--- backend/internal/pkg/validator/tag_validator.go
+++ backend/internal/pkg/validator/tag_validator.go
@@ -11,6 +11,8 @@ import (
 	govalidator "github.com/go-playground/validator/v10"
 	entranslation "github.com/go-playground/validator/v10/translations/en"
 	rutranslation "github.com/go-playground/validator/v10/translations/ru"
+
+	"skadi/backend/internal/pkg/validator/custom"
 )
 
 // Ensure tag validator implements interface.
@@ -19,15 +21,18 @@ var _ Validator = (*TagValidator)(nil)
 // TagValidator implementats [Validator] interface.
 // It validates structs by tags.
 type TagValidator struct {
-	validatorInstance *govalidator.Validate
-	translator        ut.Translator
+	validatorInst *govalidator.Validate
+	transInst     ut.Translator
+	transLocale   string
 }
 
 // NewTagValidator returns new instance of [TagValidator].
 func NewTagValidator() (*TagValidator, error) {
+	locale := "en"
+
 	enTranslator := en.New()
 	uni := ut.New(enTranslator, enTranslator)
-	trans, _ := uni.GetTranslator("en")
+	trans, _ := uni.GetTranslator(locale)
 
 	validate := govalidator.New(govalidator.WithRequiredStructEnabled())
 	err := entranslation.RegisterDefaultTranslations(validate, trans)
@@ -35,15 +40,20 @@ func NewTagValidator() (*TagValidator, error) {
 		return nil, fmt.Errorf("register translation: %w", err)
 	}
 
-	validInst := &TagValidator{validate, trans}
+	validInst := &TagValidator{validate, trans, locale}
+	if err := validInst.registerCustomValidFuncs(); err != nil {
+		return nil, err
+	}
 	return validInst, nil
 }
 
-// NewRuTagValidator returns new instance of TagValidator with ru error translation.
+// NewRuTagValidator returns new instance of [TagValidator] with ru error translation.
 func NewRuTagValidator() (*TagValidator, error) {
+	locale := "ru"
+
 	ruTranslator := ru.New()
 	uni := ut.New(ruTranslator, ruTranslator)
-	trans, _ := uni.GetTranslator("ru")
+	trans, _ := uni.GetTranslator(locale)
 
 	validate := govalidator.New(govalidator.WithRequiredStructEnabled())
 	err := rutranslation.RegisterDefaultTranslations(validate, trans)
@@ -51,13 +61,34 @@ func NewRuTagValidator() (*TagValidator, error) {
 		return nil, fmt.Errorf("register translation: %w", err)
 	}
 
-	validInst := &TagValidator{validate, trans}
+	validInst := &TagValidator{validate, trans, locale}
+	if err := validInst.registerCustomValidFuncs(); err != nil {
+		return nil, err
+	}
 	return validInst, nil
+}
+
+// registerCustomValidFuncs registers custom validate funcs for tag validator.
+func (v TagValidator) registerCustomValidFuncs() error {
+	// custom validation tags list
+	customList := []*custom.ValidTag{
+		custom.StrongPasswd,
+	}
+
+	// register all tags with translation
+	var err error
+	for _, validTag := range customList {
+		err = validTag.RegisterWithTrans(v.validatorInst, v.transInst, v.transLocale)
+		if err != nil {
+			return fmt.Errorf("register tag %s: %w", validTag.Tag, err)
+		}
+	}
+	return nil
 }
 
 // Validate validates given struct s (s is a pointer to struct).
 func (v TagValidator) Validate(s any) error {
-	err := v.validatorInstance.Struct(s)
+	err := v.validatorInst.Struct(s)
 	if err == nil { // NOT err
 		return nil
 	}
@@ -67,7 +98,7 @@ func (v TagValidator) Validate(s any) error {
 		return err
 	}
 	// handle error messages
-	rawTranstaledMap := validateErrors.Translate(v.translator)
+	rawTranstaledMap := validateErrors.Translate(v.transInst)
 
 	// sort out errors and concat them into string
 	transtaledStringSlice := make([]string, 0, len(rawTranstaledMap))
